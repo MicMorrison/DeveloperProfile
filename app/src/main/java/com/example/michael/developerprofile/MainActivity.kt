062baf1b1b934d1a8139f453c8e1c31e515de5e9
Michael Morrison <micwmorrison@gmail.com>
2018-03-14 23:32:25 -0500

    Added user interface elements

--- app/src/main/java/com/example/michael/developerprofile/MainActivity.kt
+++ app/src/main/java/com/example/michael/developerprofile/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.michael.developerprofile
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button_btn)
+
+        button.setOnClickListener {
+            Toast.makeText(this,"You clicked me!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
+
